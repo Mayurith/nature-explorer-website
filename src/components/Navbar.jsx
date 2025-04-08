ce1ec624058042ec776168c019fe1ea167a99375
Mayurith <mayuritharanga228@gmail.com>
2025-04-08 06:32:01 +0530

    add hero section ( Home page )

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import styles from '../styles/Navbar.css'
 
 function Navbar() {
   return (
     <>
-      <p>Hello</p>
+      {/* <Link to="/" className={styles.link}>Home</Link> */}
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styles from '../styles/Home.css'
 
 function Home() {
   return (
-    <div>Home
-      <p>Home page</p>
-    </div>
+    <>
+      {/* Hero Section */}
+      <section className={styles.hero}>
+        <div>
+          <h1>Discover the Beauty of Nature</h1>
+          <p>Embark on unforgettable adventures in the wild</p>
+          <button>Explore Now</button>
+        </div>
+      </section>
+    </>
   )
 }
 
